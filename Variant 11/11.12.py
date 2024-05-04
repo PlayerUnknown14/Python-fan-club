0ea14e5e9d7daaaf150c77b4ccd4ce6967fc2994
PlayerUnknown14 <gordovvladislav@yandex.ru>
2024-05-04 19:51:00 +0700

    11.12

--- Variant 11/11.12.py
+++ Variant 11/11.12.py
@@ -1,13 +1,12 @@
-T = 0
 for n in range(4, 100):
     s = '3' + n*'7'
     while '37' in s or '577' in s or '777' in s:
         if '37' in s:
-            s = s.replace('37', '7')
+            s = s.replace('37', '7', 1)
         if '577' in s:
-            s = s.replace('577', '73')        
+            s = s.replace('577', '73', 1)        
         if '777' in s:
-            s = s.replace('777', '5')
+            s = s.replace('777', '5', 1)
     sum = 0
     S = int(s)
     for i in range(len(s)):
@@ -17,7 +16,5 @@ for n in range(4, 100):
     for k in range(2, max(n, sum)):
         if sum % k == 0 and n % k == 0:
             m += 1
-    if n == 97:
-        T = 1
     if sum > 9 and sum < 100 and m == 0:
-        print(n)
+        print(n) #Answer: 97
