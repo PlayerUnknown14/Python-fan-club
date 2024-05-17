b393b140697777c99b436aa76bb1569a16937fb0
PlayerUnknown14 <139672964+PlayerUnknown14@users.noreply.github.com>
2024-05-17 08:19:09 +0700

    Update 11.23.py

--- Variant 11/11.23.py
+++ Variant 11/11.23.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-def F(x, y):
-    if x > y or x == 25 or x == 30:
-        return 0
-    if x == y:
-        return 1
-    else:
-        return F(x + 1, y) + F(x + 2, y) + F(x * 3, y)
-print(F(1, 15) * F(15, 43))
-=======
 def F(x, y, c):
     if x > y or x == 25 or x == 30 or c >= 2:
         return 0
